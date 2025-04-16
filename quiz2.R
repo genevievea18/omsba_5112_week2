efbc9087c205206668b0ddd3d068464ed5205133
genevievea18 <gavalani@seattleu.edu>
2025-04-16 09:35:01 -0700

    updated quiz

--- quiz2.R
+++ quiz2.R
@@ -6,20 +6,22 @@ nfhs <- read_dta("IAHR52FL.dta")
 #df_1 selects all variables between the household id and hv208 plus the wealth index.
 df_1<- nfhs %>%
   select(hhid:hv208, hv270)
+
 ncol(df_1)
 #48 variables in the data frame df_1
 
 #Q3:
 #df_2 holds the household id, the individuals' line numbers, and their education in single years.
-names(nfhs)
 df_2<- nfhs %>%
   select(hhid, hvidx_01:hvidx_35, hv108_01:hv108_35)
+
 ncol(df_2)
-#72 variables in the data frame df_2
+#71 variables in the data frame df_2
 
 #Q4:
 df_females <- nfhs %>%
   select(hhid, matches("^ha[0-6]_\\d{2}$"))
+
 names(df_females)
 #double check correct output with names()
 ncol(df_females)
@@ -27,13 +29,12 @@ ncol(df_females)
 
 #Q5:
 df_males <- nfhs %>%
-  select(hhid, starts_with("hb"))%>%
-  gather(key = "variable", value = "value", -hhid)%>%
-  filter(str_detect(variable, "^hb[0-6]_\\d{2}$"))
+  select(hhid, matches("^hb[0-6]_\\d{2}$"))
+
 names(df_males)
 #checking output
 ncol(df_males)
-#3 variables un df_males
+#127 variables un df_males
 
 #Q6:
 education_df2_tidy <- df_2 %>%
@@ -52,9 +53,9 @@ female_df_tidy <- df_females%>%
   spread(key = var, value = value)%>%
   select(hhid, roster_number = num, ha1, ha2, ha3)%>%
   rename( age = ha1, weight = ha2, height = ha3)%>%
-  drop_na(age, weight, height)
-#number of females:
-nrow(female_df_tidy)  
+  drop_na(age, weight, height)%>%
+  mutate(female = TRUE)
+#138592 observations
 
 #Q8:
 male_df_tidy <- df_males%>%
@@ -63,20 +64,27 @@ male_df_tidy <- df_males%>%
   spread(key = var, value = value)%>%
   select(hhid, roster_number = num, hb1, hb2, hb3)%>%
   rename( age = hb1, weight = hb2, height = hb3)%>%
-  drop_na(age, weight, height)
-#number of males:
-nrow(male_df_tidy)
+  drop_na(age, weight, height)%>%
+  mutate(female = FALSE)
+#89834 observations
 
 #Q9:
-combined_females_males <- bind_rows(females_df_tidy, males_df_tidy)
+#binding female & male dfs first:
+combined_females_males <- bind_rows(female_df_tidy, male_df_tidy)
+
+#matching the roster_num output with a leading 0 in education df to make the join easier:
+education_df2_tidy <- education_df2_tidy %>%
+  mutate(roster_number = str_pad(roster_number, width = 2, pad = "0"))
+
+#adding in education df:
 combined_data_1 <- combined_females_males%>%
   left_join(education_df2_tidy, by = c("hhid", "roster_number"))
 
+#adding in household df:
 combined_data_2 <- combined_data_1 %>%
   left_join(df_1, by = "hhid")
 
-#median age for females:
+#median age for females (TRUE) and males (FALSE):
 combined_data_2 %>%
   group_by(female)%>%
   summarise(median_age = median(age, na.rm = TRUE))
-
